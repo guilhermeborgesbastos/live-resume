ae41bf7c7cbd89a2043451de045719e4311f0154
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-03-31 20:23:31 -0300

    Added the angular flex box to the application.
     P.S: The Angular flex box it is still BETA (31th March 2020), but the 'live-resume' isn't an enterprise application. So I have decided to try it on my personal live resume.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ResumeModule } from './resume/resume.module';
 import { PageNotFoundRoutingModule } from './404/page-not-found-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-  imports: [ BrowserModule, AppRoutingModule, ResumeModule, PageNotFoundRoutingModule ],
+  imports: [ BrowserModule, AppRoutingModule, ResumeModule, PageNotFoundRoutingModule, FlexLayoutModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
