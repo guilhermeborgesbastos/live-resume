55f32add47d10644b2ea44c5a49de68d04ddd974
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-04-12 13:30:39 -0300

    Header Component
    - Updated the HeaderComponent to be able to properly update its life-cycle and properties based on me screen resolution.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit, HostListener, OnDestroy } from '@angular/core';
 import { faBars, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -7,18 +9,53 @@ import { faBars, IconDefinition } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./header.component.css']
 })
 
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy  {
+  
+  resizeObservable$ = new Subject();
   
   hasMenuToggled: boolean;
   faBars: IconDefinition;
   title: string;
 
-  constructor() { }
+  /*
+   * If you are mutating data outside of the angular context (i.e., externally),
+   * then angular will not know of the changes. You need to use ChangeDetectorRef
+   * or NgZone in your component for making angular aware of external changes and
+   * thereby triggering change detection.
+  */
+  constructor(private zone: NgZone) { }
+
+  @HostListener('window:resize', ['$event.target'])
+  onResize(target) {
+    this.zone.run(()=> this.resizeObservable$.next(target.innerWidth));
+  }
 
   ngOnInit(): void {
     this.title = 'gbastos';
     this.faBars = faBars;
-    this.hasMenuToggled = true;
+    
+    // Debounces the resize screen actions to not affect the performance of the browser,
+    // as JavaScript is a single threaded language.
+    this.resizeObservable$.pipe(debounceTime(50)).subscribe(
+      res => {
+        this.zone.run(() => this.onDetectScreenSize(res))
+      }
+    );
+
+    // Executes the necessary adjustment based on screen properties
+    this.onDetectScreenSize(window.innerWidth);
+  }
+
+  ngOnDestroy() {
+    this.resizeObservable$.unsubscribe();
+  }
+
+  /*
+   * For media types such as tablets and mobile devices, the nav-bar navigation should be
+   * collapsed by default.
+  */
+  onDetectScreenSize(innerWidth) {
+    this.hasMenuToggled = innerWidth > 1024;
   }
 
   onToggleBar() {
