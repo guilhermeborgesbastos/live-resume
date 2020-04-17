1020c849763b020efef2a96d44feaabe2a3c905e
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-04-17 08:03:46 -0300

    Welcome Component
    - Created the welcome dialog component.

---
+++ src/app/welcome/welcome-dialog/welcome-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+    selector: 'app-welcome-dialog',
+    templateUrl: './welcome-dialog.component.html',
+    styleUrls: [ 'welcome-dialog.css' ]
+})
+
+export class WelcomeDialogComponent implements OnInit {
+    
+    constructor() {}
+
+    ngOnInit() { }
+
+}
--- src/app/welcome/welcome.molule.ts
+++ src/app/welcome/welcome.molule.ts
@@ -1,9 +1,10 @@
 import { NgModule }     from '@angular/core';
 import { WelcomeComponent } from './welcome.component';
 import { WelcomeBackgroundComponent } from './welcome-background/welcome-background.component';
+import { WelcomeDialogComponent } from './welcome-dialog/welcome-dialog.component';
 
 @NgModule({
-  declarations: [ WelcomeComponent, WelcomeBackgroundComponent ],
+  declarations: [ WelcomeComponent, WelcomeBackgroundComponent, WelcomeDialogComponent ],
   exports: [ WelcomeComponent ]
 })
 
