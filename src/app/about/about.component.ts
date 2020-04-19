0ace04ffe601b6d4d903f8669cbf7eb74acaa7b4
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-04-19 13:08:27 -0300

    About Me Component
    - Created the content of the 'About me' section.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { 
+  faBiking,
+  faDumbbell,
+  faSuitcaseRolling,
+  faBook,
+  IconDefinition
+} from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub, faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +14,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  
+  title: string;
+  name: string;
+  yearsOld: number;
+
+  linkedinUrl: string;
+  githubUrl: string;
+  facebookUrl: string;
+
+  faBiking: IconDefinition;
+  faDumbbell: IconDefinition;
+  faSuitcaseRolling: IconDefinition;
+  faBook: IconDefinition;
+  faLinkedin: IconDefinition;
+  faGithub: IconDefinition;
+  faFacebook: IconDefinition;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.name = 'Guilherme Borges Bastos'
+    this.yearsOld = this.calcAge("1993-06-29"); // Sets here, your date birthday
+
+    this.linkedinUrl = 'https://www.linkedin.com/in/guilhermeborgesbastos/';
+    this.githubUrl = 'https://github.com/guilhermeborgesbastos';
+    this.facebookUrl = 'https://www.facebook.com/guilherme.borgesbastos';
+
+    this.faBiking = faBiking;
+    this.faDumbbell = faDumbbell;
+    this.faSuitcaseRolling = faSuitcaseRolling;
+    this.faBook = faBook;
+    this.faLinkedin = faLinkedin;
+    this.faGithub = faGithub;
+    this.faFacebook = faFacebook;
   }
+
+  private calcAge(dateString: string) {
+    const birthday = new Date(dateString);
+    const ageDifMs = Date.now() - birthday.getTime();
+    const ageDate = new Date(ageDifMs); // miliseconds from epoch
+    return Math.abs(ageDate.getFullYear() - 1970);
+  }
+  
 
 }
