be86ba07af0aa2bdeb0309fa6197b7add29a20f1
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-04-20 17:28:38 -0300

    Experience Component
    - Created the basic structure for the 'Experience' section;

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import {  faChevronLeft , faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub, faFacebook, IconDefinition} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent implements OnInit {
+  
+  faLinkedin: IconDefinition;
+  faGithub: IconDefinition;
+  faFacebook: IconDefinition;
+  faChevronLeft: IconDefinition;
+  faChevronRight: IconDefinition;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.faLinkedin = faLinkedin;
+    this.faGithub = faGithub;
+    this.faFacebook = faFacebook;
+    this.faChevronLeft = faChevronLeft;
+    this.faChevronRight = faChevronRight;
   }
 
 }
