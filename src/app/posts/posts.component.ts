5884019e16491708c4ba6d5d69706636c2a0e863
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-05-01 11:12:32 -0300

    Posts Component - Basic view structure.
    - Created the basic `view structure` and styling to the posts section.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { DataService } from '../core/data.service';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.scss', './posts.component responsivity.scss']
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  currentPosition: number = 0;
+  posts: any[] = [];
+  
+  constructor(
+    private dataService: DataService,
+    private renderer: Renderer2,
+    private elRef: ElementRef,
+    private library: FaIconLibrary
+  ) {
+    library.addIconPacks(fas, fab);
+  }
 
   ngOnInit(): void {
   }
 
+  onClickPrevious() {
+
+  }
+
+  onClickNext() {
+
+  }
 }
