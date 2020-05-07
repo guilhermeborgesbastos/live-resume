bd1d5f385e386b8a7bc45eefbacc326e8833cb6a
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-05-07 10:51:31 -0300

    Contact Component - Created the contact section.
    - Created the HTML structure of the contact section;
    - Translated the texts and attributes of the contact section;
    - Imported the ReactiveFormsModule to the ResumeModule;
    - Added regex validations to the contact section.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  faEnvelope, faPhone,
+  faMapMarkerAlt, IconDefinition
+} from '@fortawesome/free-solid-svg-icons';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.scss', './contact.component.responsivity.scss']
 })
+
 export class ContactComponent implements OnInit {
+
+  name: string;
+  email: string;
+  phone: string;
+  location: string;
+    
+  faEnvelope: IconDefinition;
+  faPhone: IconDefinition;
+  faMapMarkerAlt: IconDefinition;
 
   constructor() { }
 
-  ngOnInit(): void {
+  formContact: FormGroup = new FormGroup({
+    email: new FormControl('',[
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")
+    ]),
+    name: new FormControl('',[
+      Validators.required,
+      Validators.pattern("[A-zÀ-ú ]*")
+    ]),
+    message: new FormControl('',[
+      Validators.required
+      // Validators.pattern("^(?!\s*$).+")
+    ]),
+  }); 
+
+  get senderEmail(){
+    return this.formContact.get('email')
   }
 
+  get senderName(){
+    return this.formContact.get('name')
+  }
+
+  get senderMessage(){
+    return this.formContact.get('message')
+  }
+
+  ngOnInit(): void {
+    this.name = "Guilherme Borges Bastos";
+    this.email = "guilhermeborgesbastos@gmail.com";
+    this.phone = "+55 34 98400 9731";
+    this.location = "Uberaba, Minas Gerais - Brazil";
+
+    this.faEnvelope = faEnvelope;
+    this.faPhone = faPhone;
+    this.faMapMarkerAlt = faMapMarkerAlt;
+  }
+
+  submitEmail() {
+    console.log('sensing email...');
+  }
 }
