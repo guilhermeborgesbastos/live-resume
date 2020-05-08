466cd97b2240bd1e2129be62b152a93724f23410
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-05-08 16:39:33 -0300

    Page Not Found - Created the 404 page.
    - Created the template of the 404 (Page not found);
    - Applied a basic responsivity.

--- src/app/404/page-not-found.component.ts
+++ src/app/404/page-not-found.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-page-not-found',
-    templateUrl: './page-not-found.html'
+    templateUrl: './page-not-found.html',
+    styleUrls: ['./page-not-found.component.scss', './page-not-found.component.responsivity.scss']
 })
 
 export class PageNotFoundComponent implements OnInit {
