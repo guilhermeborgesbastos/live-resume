7a83679a52dc1cd1778557a783b0ea43c3cb2ad1
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-05-12 10:16:09 -0300

    SEO Improvements  - Making the application more Search Engine friendly.
    -  Set up page title, meta description, author name, keywords, charset, viewport, and robots.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'live-resume';
+export class AppComponent implements OnInit {
+
+  title: string = 'Live Resume - Guilherme Borges Bastos';
+
+  constructor(
+    private titleService: Title,
+    private metaTagService: Meta
+  ) { }
+
+  ngOnInit(): void {
+
+    this.titleService.setTitle(this.title);
+
+    this.metaTagService.addTags([
+      { name: 'keywords', content: 'Web Developer, Java Developer, Full-stack Java Developer, DevOps Engineer, Android Developer, Guilherme Borges Bastos Live Resume, Guilherme Borges Bastos Resume, Guilherme Borges Bastos CV, Curriculum Guilherme Borges Bastos, GBASTOS Resumo, gbastos live resume' },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'author', content: 'Guilherme Borges Bastos' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'date', content: '2020-05-12', scheme: 'YYYY-MM-DD' },
+      { charset: 'UTF-8' }
+    ]);
+
+    this.metaTagService.updateTag(
+      { name: 'description', content: "Hello, I'm a Full-stack Web Developer with 10+ years of experience designing web and mobile projects. Find out more in my live-resume!" }
+    );
+  }
 }
