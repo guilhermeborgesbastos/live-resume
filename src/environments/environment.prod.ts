88da4fc8c80e55a1af321ee1a26b6e01b41a9a6e
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2020-05-18 16:00:27 -0300

    Updated some dumb env. properties for testing purposes.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC2frVRAAltsVTmdwgFnATGHoVUzNHfzsM",
+    authDomain: "live-resume-a575a.firebaseapp.com",
+    databaseURL: "https://live-resume-a575a.firebaseio.com",
+    projectId: "live-resume-a575a",
+    storageBucket: "live-resume-a575a.appspot.com",
+    messagingSenderId: "681076751855",
+    appId: "1:681076751855:web:18bae3866ebfcc4fcd8a1a",
+    measurementId: "G-00VXD77WNG"
+  }
 };
