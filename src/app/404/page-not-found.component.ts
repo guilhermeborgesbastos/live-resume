3e52fbdee28b51676cafc60bf1a1805db46f11d7
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2022-01-12 17:01:04 -0300

    (fix) extract the 'page-not-found' Keyframes into a separate SCSS file

--- src/app/404/page-not-found.component.ts
+++ src/app/404/page-not-found.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit } from "@angular/core";
 @Component({
     selector: "app-page-not-found",
     templateUrl: "./page-not-found.html",
-    styleUrls: ["./page-not-found.component.scss", "./page-not-found.component.responsivity.scss"]
+    styleUrls: ["./page-not-found.component.scss", "./page-not-found.component.responsivity.scss", "./page-not-found.keyframes.component.scss"]
 })
 
 export class PageNotFoundComponent implements OnInit {
-    
+
     constructor() {}
 
     ngOnInit(): void {}
