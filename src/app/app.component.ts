da3354bbbd2671cb0be1b78bb6e97c77957b762b
Soslan Tmenov <tmenatisoslan@gmail.com>
2023-03-12 22:42:01 -0400

    changed favicon but doesn't work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Meta, Title } from "@angular/platform-browser";
 })
 export class AppComponent implements OnInit {
 
-  title: string = "Live Resume - Guilherme Borges Bastos";
+  title: string = "Live Resume - Soslan Tmenov";
 
   constructor(
     private titleService: Title,
@@ -20,16 +20,16 @@ export class AppComponent implements OnInit {
     this.titleService.setTitle(this.title);
 
     this.metaTagService.addTags([
-      { name: "keywords", content: "Web Developer, Java Developer, Full-stack Java Developer, DevOps Engineer, Android Developer, Guilherme Borges Bastos Live Resume, Guilherme Borges Bastos Resume, Guilherme Borges Bastos CV, Curriculum Guilherme Borges Bastos, GBASTOS Resumo, gbastos live resume" },
+      { name: "keywords", content: "Web Developer, Full-stack Software Engineer, Soslan Tmenov Live Resume, Soslan Tmenov Resume," },
       { name: "robots", content: "index, follow" },
-      { name: "author", content: "Guilherme Borges Bastos" },
+      { name: "author", content: "Soslan Tmenov" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { name: "date", content: "2020-05-12", scheme: "YYYY-MM-DD" },
       { charset: "UTF-8" }
     ]);
 
     this.metaTagService.updateTag(
-      { name: "description", content: "Hello, I'm a Full-stack Web Developer with 10+ years of experience designing web and mobile projects. Find out more in my live-resume!" }
+      { name: "description", content: "Hello, I'm a Full-stack Software Engineer with 1+ years of experience designing web projects. Find out more in my live-resume!" }
     );
   }
 }
