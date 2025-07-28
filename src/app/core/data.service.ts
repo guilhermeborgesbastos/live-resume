a29c02d9080ba7467d4f1f5ba04efc55d16b0a06
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2025-07-28 20:31:34 +0200

    (feat) updating RxJS to use throwError instead

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
-import { Observable } from "rxjs";
+import { Observable, throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 
 import { IExperience } from "../experience/experience-interfaces";
@@ -40,10 +40,10 @@ export class DataService {
       console.error("server error:", error);
       if (error.error instanceof Error) {
           const errMessage = error.error.message;
-          return Observable.throw(errMessage);
+          return throwError(() => errMessage);
           // Use the following instead if using lite-server
           // return Observable.throw(err.text() || "backend server error");
       }
-      return Observable.throw(error || "Node.js server error");
+      return throwError(() => error || "Node.js server error");
     }
 }
