a616529bf5de7fb9a845d651ede768bba6d5e75e
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2025-07-31 21:28:46 +0200

    (feat) update Firebase dependencies and its implementation to support Angular 16

--- src/app/contact/contact.service.ts
+++ src/app/contact/contact.service.ts
@@ -1,13 +1,14 @@
-import { AngularFirestore, DocumentReference } from "@angular/fire/compat/firestore";
-import { Contact } from "../model/contact.model";
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
+import { Firestore, collection, addDoc, CollectionReference, DocumentReference, docData, doc } from '@angular/fire/firestore';
+import { Contact } from '../model/contact.model';
 
-@Injectable({ providedIn: "root" })
+@Injectable({ providedIn: 'root' })
 export class ContactService {
+  constructor(private firestore: Firestore) {}
 
-    constructor(private database: AngularFirestore) { }
-
-    createContact(contact: Contact): Promise<DocumentReference> {
-        return this.database.collection<Contact>("contacts").add(contact);
-    }
+  // Add a new contact
+  createContact(contact: Contact): Promise<DocumentReference<Contact>> {
+    const contactsCollection = collection(this.firestore, 'contacts') as CollectionReference<Contact>;
+    return addDoc(contactsCollection, contact);
+  }
 }
